c77130399aa5feb3623db96493c991a302b62b92
Osman9449 <104346788+Osman9449@users.noreply.github.com>
2023-09-04 21:49:28 +0400

    DSA and Algorithms

---
+++ src/LinkedList/LinkedList.java
@@ -0,0 +1,152 @@
+package LinkedList;
+
+public class LinkedList {
+    private Node head;
+    private Node tail;
+    private int length;
+
+    static class Node {
+        int value;
+        Node next;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public LinkedList(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+    }
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.value + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public void getHead() {
+        System.out.println("Head :" + head.value);
+    }
+
+    public void getTail() {
+        System.out.println("Tail :" + tail.value);
+    }
+
+    public void getLength() {
+        System.out.println("Length :" + length);
+    }
+
+
+    public Node removeLast() { // return last element
+        if (length == 0) return null; //
+        Node temp = head;
+        Node pre = head;
+        while (temp.next != null) {
+            pre = temp;
+            temp = temp.next;
+        }
+        // temp = last element of ll , pre one before from temp (last element)
+        tail = pre;
+        tail.next = null;
+        length--;
+
+        if (length == 0) { // ll length is 1 before removing last element
+            head = null;
+            tail = null;
+        }
+        return temp;
+    }
+    public Node removeFirst() {// return first element
+        if (length == 0) return null;
+        Node temp = head;
+        head = head.next;
+        temp.next = null; // remove temp
+        length--;
+        if (length == 0) { // ll length is 1 before removing last element
+            tail = null; // head.next =0 , tail only have value
+        }
+        return temp; // removed element
+    }
+
+    public void append(int value) {
+        Node newNode = new Node(value);
+        if (length == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+        length++;
+    }
+
+    public void prepend(int value) {
+        Node newNode = new Node(value);
+        if (length == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
+        length++;
+    }
+
+    public Node get(int index) {
+        if (index < 0 || index >= length) {
+            return null;
+        }
+        Node temp = head;
+        for (int i = 0; i < index; i++) {
+            temp = temp.next;
+        }
+        return temp;
+    }
+    public boolean set (int index ,int value){
+        Node temp = get(index);
+        if(temp == null){
+            return false;
+        }
+        temp.value=value;
+        return true;
+    }
+
+    public boolean insert (int index, int value)  {
+        if (index < 0 || index > length) return false;
+        if (index == 0) {
+            prepend(value);
+            return true;
+        }
+        if (index == length) {
+            append(value);
+            return true;
+        }
+        Node newNode = new Node(value);
+        Node temp = get(index - 1);
+        newNode.next = temp.next;
+        temp.next = newNode;
+        length++;
+        return true;
+    }
+    public Node remove(int index) {  // return removed node
+        if (index < 0 || index >= length) return null;
+        if (index == 0) return removeFirst();
+        if (index == length - 1) return removeLast();
+
+        Node prev = get(index - 1);
+        Node temp = prev.next;
+
+        prev.next = temp.next;
+        temp.next = null;
+        length--;
+        return temp;
+    }
+
+
+}
---
+++ src/LinkedList/Main.java
@@ -0,0 +1,11 @@
+package LinkedList;
+
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList linkedList = new LinkedList(5);
+        System.out.print(linkedList.removeFirst().value);
+
+    }
+
+}
